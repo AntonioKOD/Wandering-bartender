9bd158d56bbb6561dd23b4024456b5fb008e1d70
Your Name <you@example.com>
2025-01-13 09:41:38 -0500

    changed name

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import wandering from '@/public/wandering.svg'
+import wandering from '@/public/Wandering.svg'
 
 
 export default function NavBar(){
